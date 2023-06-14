a2569ebc40b4bda6ebcf0fa77c7adf9cc1b8eb11
Sitha Sovannara <sithasovannara51@gmail.com>
2023-06-14 17:56:37 +0700

    Added tag filter functionality

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -7,10 +7,14 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    public function index () 
+    public function index (Request $request) 
     {
+        // dependency injection or helper function (to get the query parameter)
+        // filter listing tag by scope filter
+
+        // dd($request->tag);
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag']))->get()
         ]);
     }
 
