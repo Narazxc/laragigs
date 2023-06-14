c43e1abb0ef81cfc83d9c1db7a303ebbd715a99c
Sitha Sovannara <sithasovannara51@gmail.com>
2023-06-14 17:30:01 +0700

    Just another way of doing things, turned layout into component that can wraps around other components/template

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,7 +1,4 @@
-@extends('layout')
-
-
-@section('content')
+<x-layout>
     @include('partials._hero')
     @include('partials._search')
 
@@ -16,4 +13,4 @@
             @endforeach
         @endif
     </div>
-@endsection
+</x-layout>
